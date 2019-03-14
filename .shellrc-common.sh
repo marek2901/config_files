d9ca6103f09487d87d9bd11567994caee9cb8dca
Marek Dziewit <marek2901@gmail.com>
2019-03-14 01:38:21 +0100

    Rbenv git install compatibility

--- .shellrc-common.sh
+++ .shellrc-common.sh
@@ -78,7 +78,12 @@ fi
 ###
 
 ### ruby Rbenv
-if which rbenv &> /dev/null; then eval "$(rbenv init -)"; fi
+if [[ -d  "$HOME/.rbenv/bin" ]]; then
+    export PATH="$HOME/.rbenv/bin:$PATH"
+fi
+if which rbenv &> /dev/null; then
+  eval "$(rbenv init -)";
+fi
 ###
 
 ### python PyEnv
