23eb0d6eb386edc169400b83683d49efbea9ecf1
Marek Dziewit <marek.dziewit@automatic.com>
2019-04-25 17:35:08 -0700

    Add mitmproxy alias to drop mouse support

--- .shellrc-common.sh
+++ .shellrc-common.sh
@@ -4,6 +4,10 @@ alias la='ls -A'
 alias l='ls -CF'
 
 alias ifmyip4="ifconfig | grep -E 'inet\ \d+.*broadcast' | grep -Eo 'inet \d+\.\d+\.\d+\.\d+' | cat"
+
+if which mitmproxy &> /dev/null; then
+  alias mitmproxy="mitmproxy --set console_mouse=false "
+fi
 ########## GENERIC ALIASES END
 
 ########## GIT ALIASES
