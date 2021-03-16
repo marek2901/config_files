278dcce0bdea267d7021a71213f8e4202e03a351
Marek Dziewit <marek2901@gmail.com>
2021-03-16 19:36:44 +0100

    Add sdk man

--- .shellrc-common.sh
+++ .shellrc-common.sh
@@ -226,3 +226,10 @@ if which phpenv &> /dev/null; then
   eval "$(phpenv init -)";
 fi
 ###
+
+### Java SDK Man
+if [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]]; then
+  export SDKMAN_DIR="$HOME/.sdkman"
+  source "$HOME/.sdkman/bin/sdkman-init.sh"
+fi
+###
